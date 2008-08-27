19d946ad1710a1da19c179abcc97370d1c34f5bc
Simon Willison <simon@simonwillison.net>
2008-08-27 21:57:05 +0000

    Use new base65 encoding scheme for signing cookies using less bytes of signature

--- django_openid/utils.py
+++ django_openid/utils.py
@@ -31,7 +31,7 @@ def encode_object(obj, secret=None):
     pickled = pickle.dumps(obj)
     compressed = zlib.compress(pickled)
     base64d = base64.urlsafe_b64encode(compressed)
-    sig = hashlib.sha1(base64d + (secret or settings.SECRET_KEY)).hexdigest()
+    sig = sign(base64d, (secret or settings.SECRET_KEY))
     return base64d + ':' + sig
 
 def decode_object(s, secret=None):
@@ -39,10 +39,53 @@ def decode_object(s, secret=None):
     s = s.encode('utf8') # base64 works on bytestrings, not on unicodes
     if s.count(':') != 1:
         raise ValueError, 'Should be one and only one colon'
-    base64d, sig1 = s.split(':')
-    sig2 = hashlib.sha1(base64d + (secret or settings.SECRET_KEY)).hexdigest()
-    if sig1 != sig2:
-        raise ValueError, 'Signature failed: %s != %s' % (sig1, sig2)
+    base64d, sig = s.split(':')
+    if not verify(base64d, (secret or settings.SECRET_KEY), sig):
+        raise ValueError, 'Signature failed: %s' % sig
     compressed = base64.urlsafe_b64decode(base64d)
     pickled = zlib.decompress(compressed)
     return pickle.loads(pickled)
+
+
+# Utility functions for signing a string using SHA1, then shrinking that SHA1
+# hash down to as short as possible using lossless base65 compression.
+
+BASE10 = "0123456789"
+# Characters that are NOT encoded by urllib.urlencode:
+URLSAFE = '-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
+
+import hashlib
+
+def sign(s, key):
+    return base65_sha1(s + ':'  + key)
+
+def verify(s, key, sig):
+    return sign(s, key) == sig
+
+def base65_sha1(s):
+    return int_to_base65(int(hashlib.sha1(s).hexdigest(), 16))
+
+def sha1_from_base65(s):
+    i = base65_to_int(s)
+    return hex(i).replace('0x', '')
+
+def int_to_base65(i):
+    return baseconvert(str(i).lower().replace('L', ''), BASE10, URLSAFE)
+
+def base65_to_int(s):
+    return baseconvert(s, URLSAFE, BASE10)
+
+def baseconvert(number_string, from_digits, to_digits):
+    "Convert a number between two bases of arbitrary digits"
+    # Inspired by http://code.activestate.com/recipes/111286/
+    # Convert number_string (in from_digits encoding) to an integer
+    i = 0L
+    for digit in str(number_string):
+       i = i * len(from_digits) + from_digits.index(digit)
+    # Convert integer to to_digits encoding
+    res = []
+    while i > 0:
+        res.insert(0, to_digits[i % len(to_digits)])
+        i = i / len(to_digits)
+    return ''.join(res)
+
