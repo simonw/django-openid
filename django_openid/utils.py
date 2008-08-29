0625c1906a11a1bd98f7d01b248048c98c944dda
Simon Willison <simon@simonwillison.net>
2008-08-29 08:53:49 +0000

    Made signing more robust, although I'll probably replace it with base64 very shortly

--- django_openid/utils.py
+++ django_openid/utils.py
@@ -26,22 +26,23 @@ class OpenID:
         )
 
 # Functions for converting an object in to a signed base64 urlsafe string
-def encode_object(obj, secret=None):
-    "Returns URL-safe, sha1 signed base64 compressed pickle"
+def encode_object(obj, secret = None):
+    """
+    Returns URL-safe, sha1 signed base64 compressed pickle. If secret is 
+    None, settings.SECRET_KEY is used instead.
+    """
     pickled = pickle.dumps(obj)
     compressed = zlib.compress(pickled)
     base64d = base64.urlsafe_b64encode(compressed)
-    sig = sign(base64d, (secret or settings.SECRET_KEY))
-    return base64d + ':' + sig
+    return sign(base64d, secret)
 
-def decode_object(s, secret=None):
+def decode_object(s, secret = None):
     "Reverse of encode_object(), raises ValueError if signature fails"
     s = s.encode('utf8') # base64 works on bytestrings, not on unicodes
-    if s.count(':') != 1:
-        raise ValueError, 'Should be one and only one colon'
-    base64d, sig = s.split(':')
-    if not verify(base64d, (secret or settings.SECRET_KEY), sig):
-        raise ValueError, 'Signature failed: %s' % sig
+    try:
+        base64d = unsign(s, secret)
+    except ValueError:
+        raise
     compressed = base64.urlsafe_b64decode(base64d)
     pickled = zlib.decompress(compressed)
     return pickle.loads(pickled)
@@ -56,24 +57,54 @@ URLSAFE = '-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz'
 
 import hashlib
 
-def sign(s, key):
-    return base65_sha1(s + ':'  + key)
+class BadSignature(ValueError):
+    # Extends ValueError, which makes it more convenient to catch
+    pass
 
-def verify(s, key, sig):
-    return sign(s, key) == sig
+def sign(value, key = None):
+    if isinstance(value, unicode):
+        raise TypeError, \
+            'sign() needs bytestring, not unicode: %s' % repr(value)
+    if key is None:
+        key = settings.SECRET_KEY
+    return value + ':' + base65_sha1(value + key)
+
+def unsign(signed_value, key = None):
+    if isinstance(signed_value, unicode):
+        raise TypeError, 'unsign() needs bytestring, not unicode'
+    if key is None:
+        key = settings.SECRET_KEY
+    if not ':' in signed_value:
+        raise BadSignature, 'Missing sig (no : found in value)'
+    value, sig = signed_value.rsplit(':', 1)
+    if base65_sha1(value + key) == sig:
+        return value
+    else:
+        raise BadSignature, 'Signature failed: %s' % sig
 
 def base65_sha1(s):
     return int_to_base65(int(hashlib.sha1(s).hexdigest(), 16))
 
 def sha1_from_base65(s):
     i = base65_to_int(s)
-    return hex(i).replace('0x', '')
+    in_hex = hex(i)
+    # Tricky bug (discovered through unit tests) - we might get a value like
+    # '0xb2e5c3e38924d1ac9ab7c7adea91536d3d1d215L' - which we need to change 
+    # back in to a more traditional looking 40 char sha1
+    
+    # Remove leading '0x'
+    in_hex = in_hex.replace('0x', '')
+    # ... and the trailing L:
+    in_hex = in_hex.replace('L', '')
+    # And if it's less than 40 chars, add leading '0's as padding
+    in_hex = ((40 - len(in_hex)) * '0') + in_hex
+    return in_hex
 
 def int_to_base65(i):
     return baseconvert(str(i).lower().replace('L', ''), BASE10, URLSAFE)
 
 def base65_to_int(s):
-    return baseconvert(s, URLSAFE, BASE10)
+    return int(baseconvert(s, URLSAFE, BASE10))
 
 def baseconvert(number_string, from_digits, to_digits):
     "Convert a number between two bases of arbitrary digits"
