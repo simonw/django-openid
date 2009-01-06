50db89105dc24dfdef1cb0c16344b3e80c4bafe1
Brian Rosner <brosner@gmail.com>
2009-01-07 01:28:21 +0800

    Fixed issue #22 -- Added RequestContext to render methods.
    Signed-off-by: Simon Willison <simon@simonwillison.net>

--- django_openid/consumer.py
+++ django_openid/consumer.py
@@ -10,6 +10,8 @@ urlpatterns = patterns('',
     ...
 )
 """
+from django.conf import settings
+from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from openid.consumer import consumer
--- django_openid/provider.py
+++ django_openid/provider.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.template import RequestContext
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response
 from openid.server.server import Server
