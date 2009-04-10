aa140b0499e4ce00f8bb3760234c9a565e16264b
Simon Willison <simon@simonwillison.net>
2009-04-10 15:27:34 +0000

    Ported documentation to Sphinx and added (auto generated) API docs - the non-API-docs still need a lot of work

---
+++ django_openid/docs/fake_project/settings.py
@@ -0,0 +1,10 @@
+# Fake settings.py so we can get sphinx autodoc to run against Django without 
+# complaining.
+
+DEBUG = True
+TEMPLATE_DEBUG = True
+
+DATABASE_ENGINE = ''
+DATABASE_NAME = ''
+DATABASE_USER = ''
+
